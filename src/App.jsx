6fe39ecb4be655a417aa9291e1714019d19766f3
Kirti <kirtipadhi06@gmail.com>
2024-08-14 00:38:06 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { React } from "react";
+
+import "./App.css";
+
+// import Product from "./components/Product";
+
+import { Link, Route, Switch, NavLink } from "react-router-dom";
+
+import Home from "./components/Home";
+import Product from "./components/Product";
+import PageNotFound from "./components/PageNotFound";
+
+const App = () => (
+    <>
+        <div className="flex space-x-2">
+            <NavLink exact activeClassName="underline font-bold" to="/">
+                Home
+            </NavLink>
+            <NavLink exact activeClassName="underline font-bold" to="/product">
+                Product
+            </NavLink>
+        </div>
+        <Switch>
+            <Route exact component={Home} path="/" />
+            <Route exact component={Product} path="/product" />
+            <Route component={PageNotFound} path="*" />
+        </Switch>
+    </>
+);
+
+export default App;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,77 @@
+import classNames from "classnames";
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+import { useState, useEffect, useRef } from "react";
+
+const Carousel = ({ imageUrls, title }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNext = () =>
+        setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+
+    const handlePrevious = () => {
+        setCurrentIndex(
+            prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+        );
+        resetTimer();
+    }
+
+    const timerRef = useRef(null);
+
+    useEffect(() => {
+        timerRef.current = setInterval(handleNext, 3000);
+
+        return () => clearInterval(timerRef.current);
+    }, []);
+
+    const resetTimer = () => {
+        clearInterval(timerRef.current);
+        timerRef.current = setInterval(handleNext, 3000);
+    };
+
+    return (
+        <div className="flex flex-col items-center">
+            <div className="flex items-center">
+                <Button
+                    className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+                    icon={Left}
+                    style="text"
+                    onClick={handlePrevious}
+                />
+                <img
+                    alt={title}
+                    className="max-w-56 h-56 max-h-56 w-56"
+                    src={imageUrls[currentIndex]}
+                />
+                <Button
+                    className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+                    icon={Right}
+                    style="text"
+                    onClick={() => {
+                        handleNext();
+                        resetTimer();
+                      }}
+                />
+            </div>
+            <div className="flex space-x-1 pt-5">
+                {imageUrls.map((_, index) => {
+                    return (
+                        <span
+                            key={index}
+                            className={classNames(
+                                "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+                                { "neeto-ui-bg-black": index === currentIndex }
+                            )}
+                            onClick={() => {
+                                setCurrentIndex(index);
+                                resetTimer();
+                            }}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
